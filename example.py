afec053314aebf44d22adabbabef035950c6577b
Ferran Llamas <ferran@onna.com>
2021-06-29 16:02:37 +0200

    better example

--- example.py
+++ example.py
@@ -1,6 +1,15 @@
-from pyflipt import get_client
-from pyflipt import Flag, Segment, Constraint, Rule, ComparisonType, MatchType, OperatorType
 import asyncio
+
+from pyflipt import (
+    ComparisonType,
+    Constraint,
+    Flag,
+    MatchType,
+    OperatorType,
+    Rule,
+    Segment,
+    get_client,
+)
 
 FLIPT_API_BASE_URL = "http://localhost:8083/api/v1"
 
@@ -8,34 +17,45 @@ FLIPT_API_BASE_URL = "http://localhost:8083/api/v1"
 async def main():
     pf = get_client(FLIPT_API_BASE_URL)
 
-    flag = Flag(key="collectors3", name="Collectors", enabled=True)
+    flag = Flag(key="myflag", name="My Flag", enabled=True)
     await pf.create(flag)
 
     segments = [
-        Segment(key="user", name="Selected users",  match_type=MatchType.ANY.value),
-        Segment(key="account", name="Selected accounts",  match_type=MatchType.ANY.value),
-        Segment(key="datasource_uuid", name="Selected datasources",  match_type=MatchType.ANY.value),
+        Segment(key="user", name="Selected users", match_type=MatchType.ANY.value),
+        Segment(
+            key="account", name="Selected accounts", match_type=MatchType.ANY.value
+        ),
     ]
     for segment in segments:
         await pf.create(segment)
 
     constraints = [
-        Constraint(segment_key=segments[0].key, type=ComparisonType.STRING.value, property="user", operator=OperatorType.EQ, value="test.admin@onna.com"),
-        Constraint(segment_key=segments[1].key, type=ComparisonType.STRING.value, property="account", operator=OperatorType.EQ, value="account1"),
-        Constraint(segment_key=segments[2].key, type=ComparisonType.STRING.value, property="datasource_uuid", operator=OperatorType.EQ, value="foobar"),
+        Constraint(
+            segment_key=segments[0].key,
+            type=ComparisonType.STRING.value,
+            property="user",
+            operator=OperatorType.EQ,
+            value="user@mailbox.org",
+        ),
+        Constraint(
+            segment_key=segments[1].key,
+            type=ComparisonType.STRING.value,
+            property="account",
+            operator=OperatorType.EQ,
+            value="some-client-account",
+        ),
     ]
     for constraint in constraints:
         await pf.create(constraint)
 
     rules = []
     for rank, segment in enumerate(segments):
-        rules.append(
-            Rule(flag_key=flag.key, segment_key=segment.key, rank=rank + 1)
-        )
+        rules.append(Rule(flag_key=flag.key, segment_key=segment.key, rank=rank + 1))
     for rule in rules:
         await pf.create(rule)
 
     await pf.close()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
